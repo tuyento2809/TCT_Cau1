dbd01656a9c5dfa24bc7fd9d10087c896d1fecee
To Cong Tuyen <tuyento2809@gmail.com>
2019-08-26 13:42:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/student/tct_cau1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.student.tct_cau1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextA, editTextB;
+    TextView textViewKQ;
+    Button buttonKQ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextA = findViewById(R.id.editTextA);
+        editTextB = findViewById(R.id.editTextB);
+        textViewKQ = findViewById(R.id.textViewKQ);
+        buttonKQ = findViewById(R.id.buttonKQ);
+
+        buttonKQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(editTextA.getText().toString());
+                int b = Integer.parseInt(editTextB.getText().toString());
+                int kq = a+b;
+                textViewKQ.setText("Ket qua: "+kq);
+            }
+        });
+
+    }
+}
